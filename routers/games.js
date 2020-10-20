cb3997daadfba23149ea345ac777d1e9f7c00fbc
Bloodorange690 <bluespruce185@gmail.com>
2020-10-20 17:10:49 +0200

    added patch router in order to change genres in game

--- routers/games.js
+++ routers/games.js
@@ -35,4 +35,22 @@ router.post("/:code", async (req, res, next) => {
   res.status(200).send(game);
 });
 
+router.patch("/:id", async (req, res, next) => {
+  const gameToUpdate = await Game.findByPk(req.params.id);
+  const { genres } = req.body;
+  await gameToUpdate.update({ genres });
+
+  const moviesResponse = await axios.get(`
+  https://api.themoviedb.org/3/discover/movie?      api_key=0f9d53792bd5deaacc609c8e93ec4148&&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${genres.join(',')}`)
+
+  const myTenMovies = moviesResponse.results.slice(10);
+
+  myTenMovies.map(m => {
+    await GameMovie.create({ gameId: gameToUpdate.id, movieId: m.id })
+  })
+
+  console.log("req body", req.body);
+
+  return res.status(200).send(gameToUpdate);
+});
 module.exports = router;
